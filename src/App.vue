a80b986b11ed383cc7fe75474331bba8d9f3b0d6
eliotl <23159423+eliotl@users.noreply.github.com>
2022-07-19 11:55:42 -0700

    Added pinia and store

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,19 @@ Journey
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 import Journey from './components/Journey.vue';
+import { useJourneyStore } from '@/store/index';
 
 @Options({
   components: {
     Journey,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  setup() {
+    const journeyStore = useJourneyStore();
+    return journeyStore;
+  }
+}
 
 
 </script>
